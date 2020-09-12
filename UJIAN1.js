ef5ba05b611b53bed48f34deaa8c62c93e9f4405
faizanshar <muhammadfaizanshar@gmai.com>
2020-09-12 07:14:38 +0700

    first commit

---
+++ UJIAN1.js
@@ -0,0 +1,12 @@
+var nilaiSantri = [50,90,65,67,89,80,50,97,84]
+
+var total =0;
+var bykNilai = nilaiSantri.length;
+
+ for(i = 0; i < nilaiSantri.length; i++){
+     console.log(i)
+     total += nilaiSantri[i]
+
+ }
+ document.write(total/bykNilai)
+ 
